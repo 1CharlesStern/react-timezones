f34f5a8cc6a44277d8e9e31fc98eaf89d31e7f61
Charles Stern <1charlesstern@gmail.com>
2024-09-03 18:53:08 -0400

    Default datetime clock functioning

--- components/DateComparer/DateComparer.tsx
+++ components/DateComparer/DateComparer.tsx
@@ -1,7 +1,24 @@
+'use client'
+import { DateTimePicker } from '@mantine/dates'
+import { ChangeEvent, useState } from 'react'
+
 export function DateComparer() {
+  const [defaultDate, setDefault] = useState<Date>()
+  let userDate = null
+
+  const i = setInterval(() => {
+    setDefault(new Date())
+  })
+
   return (
     <>
-      <div>Hello World!</div>
+      <DateTimePicker
+        onChange={(d: Date) => { clearInterval(i); userDate = d }}
+        value={userDate || defaultDate}
+        label="Date & Time at your current location"
+        valueFormat="DD MMM YYYY hh:mm:ss A" 
+      />
+      <DateTimePicker label="Date & Time at your destination" />
     </>
   )
 }
