64646be4fb4f9212f9e6ff29656808a3fc0a4257
Charles Stern <1charlesstern@gmail.com>
2024-09-19 11:30:56 -0400

    Timezones now populate automatically

--- components/TimeZonePicker/TimeZonePicker.tsx
+++ components/TimeZonePicker/TimeZonePicker.tsx
@@ -1,19 +1,32 @@
 import { Autocomplete } from '@mantine/core'
 import { useEffect, useState } from 'react'
+import dayjs from 'dayjs'
+import timezone from 'dayjs/plugin/timezone'
+import utc from 'dayjs/plugin/utc'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
 type Props = {
   onChange: Function
 }
 
 export default function TimeZonePicker ({ onChange }: Props) {
+  const [value, setValue] = useState(dayjs.tz.guess())
   const tzData = Intl.supportedValuesOf('timeZone')
+
+  const handleChange = (tz: string) => {
+    setValue(tz)
+    onChange(tz)
+  }
 
   return (
     <>
       <Autocomplete
         data={tzData}
+        value={value}
         label="Select Timezone"
-        onChange={onChange}
+        onChange={handleChange}
       />
     </>
   )
