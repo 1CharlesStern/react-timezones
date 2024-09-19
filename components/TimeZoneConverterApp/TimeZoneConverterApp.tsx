739ce65664c700917087174d81d890c3fe814e74
Charles Stern <1charlesstern@gmail.com>
2024-09-19 12:32:24 -0400

    Cleaned up errors

--- components/TimeZoneConverterApp/TimeZoneConverterApp.tsx
+++ components/TimeZoneConverterApp/TimeZoneConverterApp.tsx
@@ -2,22 +2,21 @@
 
 import { useState } from 'react'
 import { Container, Grid } from '@mantine/core'
-import SmartDateSelector from '@/components/SmartDateSelector/SmartDateSelector'
-import TimeZonePicker from '@/components/TimeZonePicker/TimeZonePicker'
+import dynamic from 'next/dynamic'
 import dayjs from 'dayjs'
+import timezone from 'dayjs/plugin/timezone'
+import utc from 'dayjs/plugin/utc'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
+const SmartDateSelector = dynamic(() => import('@/components/SmartDateSelector/SmartDateSelector'), { ssr: false })
+const TimeZonePicker = dynamic(() => import('@/components/TimeZonePicker/TimeZonePicker'), { ssr: false })
+
 
 export default function TimeZoneConverterApp () {
   const [fromTz, setFromTz] = useState<string>(dayjs.tz.guess())
   const [toTz, setToTz] = useState<string>(dayjs.tz.guess())
-  const fromChange = (tz: string) => {
-    // do conversion
-    setFromTz(tz)
-  }
-
-  const toChange = (tz: string) => {
-    // do conversion
-    setToTz(tz)
-  }
 
   return (
     <>
@@ -25,7 +24,7 @@ export default function TimeZoneConverterApp () {
         <Grid>
           <Grid.Col span={2}>
             <TimeZonePicker 
-              onChange={fromChange}
+              onChange={setFromTz}
             />
           </Grid.Col>
           <Grid.Col span={3} offset={1}>
@@ -42,7 +41,7 @@ export default function TimeZoneConverterApp () {
           </Grid.Col>
           <Grid.Col span={2}>
             <TimeZonePicker
-              onChange={toChange}
+              onChange={setToTz}
             />
           </Grid.Col>
         </Grid>
